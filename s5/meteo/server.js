0477264f7d1ea45c0f915f9ad37f51845e58b6af
Bogdan Tatu <bogdanntatu@yahoo.com>
2020-10-28 11:20:46 +0200

    Seminar 5

---
+++ s5/meteo/server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const fetch = require('node-fetch')
+
+const WEATHER_URL = 'http://www.meteoromania.ro/wp-json/meteoapi/v2/starea-vremii'
+
+const app=express()
+
+app.use(express.static('public'))
+
+app.get('/weather', async (req, res) => {
+    try {
+        const response = await fetch (WEATHER_URL)
+        const responseBody = await response.json()
+        if(req.query.city){
+            const weatherRecord = responseBody.features.find((e) =>
+            e.properties.nume.indexOf(req.query.city.toUpperCase()) !== -1)
+            if(weatherRecord){
+                res.status(200).json(weatherRecord)
+            }
+            else{
+                res.status(404).json({ message: 'Not found' })
+            }
+        }
+        else{
+            res.status(200).json(responseBody)
+        }
+        
+    } catch (error) {
+        console.warn(error)
+        res.status(500).json({ message: 'server error' })
+    }
+})
+
+app.listen(8000)
